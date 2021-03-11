4e946ca67b6c16fd80e0231326c734e7c2b65e49
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-03-11 17:29:04 +0100

    Wielkość liter w symbolu generatora nie ma znaczenia

--- 1. Sortowanie/generate.py
+++ 1. Sortowanie/generate.py
@@ -49,7 +49,7 @@ def get_generator_func():
     choice = ''
     while True:
         print('Wybierz rodzaj zestawu danych [', ' '.join(legal_types.keys()), ']: (? to pomoc) ', end='', sep='')
-        choice = input().strip()
+        choice = input().strip().upper()
         if choice in legal_types:
             break
 
