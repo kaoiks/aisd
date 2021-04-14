0b5175c7f18f75929d93eebf2f098bcfd3017329
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-04-14 18:20:34 +0200

    Dodano graf reprezentowany listą następników

---
+++ 3. Grafy/representations/list_graph.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class ListGraph:
+    def __init__(self, vertices):
+        self.vertices = [None] * len(vertices)
+        for i in range(len(vertices)):
+            self.vertices[vertices[i].id] = ListGraphNode(vertices[i].id)
+        
+        for i in range(len(vertices)):
+            for node in vertices[i].successors:
+                self.vertices[vertices[i].id].addSuccessor(node)
+    
+    def getVertex(self, num):
+        return self.vertices[num]
+    
+    def getVertexCount(self):
+        return len(self.vertices)
+
+
+class ListGraphNode:
+    def __init__(self, val):
+        self.successors = deque()
+        self.id = val
+
+    def addSuccessor(self, succ):
+        self.successors.append(succ)
+    
+    def getImmediateSuccessors(self):
+        return self.successors
