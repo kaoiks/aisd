a676bd11e1b1427601e2de246bfbb2058eba8f53
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-04-15 16:17:54 +0200

    Dodaję reprezentację macierzową grafu

---
+++ 3. Grafy/representations/matrix_graph.py
@@ -0,0 +1,20 @@
+class MatrixGraph:
+
+    def __init__(self, vertices):
+        self.adjacency = []
+        for _ in range(len(vertices)):
+            self.adjacency.append([False] * len(vertices))
+
+        for i in range(len(vertices)):
+            for succ in vertices[i].successors:
+                self.adjacency[i][succ.id] = True
+
+    def getVertexCount(self):
+        return len(self.adjacency)
+
+    def getImmediateSuccessors(self, index):
+        succ = []
+        for i in range(len(self.adjacency[index])):
+            if self.adjacency[index][i]:
+                succ.append(i)
+        return succ
