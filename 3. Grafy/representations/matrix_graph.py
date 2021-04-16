f34d9c197adf64bdb03ec539612f0585ed1323ed
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-04-16 12:18:26 +0200

    Zoptymalizowano tworzenie tablic

--- 3. Grafy/representations/matrix_graph.py
+++ 3. Grafy/representations/matrix_graph.py
@@ -1,20 +1,24 @@
 class MatrixGraph:
 
     def __init__(self, vertices):
-        self.adjacency = []
-        for _ in range(len(vertices)):
-            self.adjacency.append([False] * len(vertices))
+        self.adjacency = [None] * len(vertices)
+        self.succ_cnt = [0] * len(vertices)
+        for i in range(len(vertices)):
+            self.adjacency[i] = [False] * len(vertices)
 
         for i in range(len(vertices)):
             for succ in vertices[i].successors:
                 self.adjacency[i][succ.id] = True
+                self.succ_cnt[i] += 1
 
     def getVertexCount(self):
         return len(self.adjacency)
 
     def getImmediateSuccessors(self, index):
-        succ = []
+        succ = [0] * self.succ_cnt[index]
+        j = 0
         for i in range(len(self.adjacency[index])):
             if self.adjacency[index][i]:
-                succ.append(i)
+                succ[j] = i
+                j += 1
         return succ
