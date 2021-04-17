308843b497e1ddc04e2c7cdca48a66f2f6d716ff
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-04-17 17:49:12 +0200

    Dodaję opis użycia

--- 3. Grafy/gen.py
+++ 3. Grafy/gen.py
@@ -3,8 +3,13 @@ import sys
 from generator.dag_generator import generateDag
 from generator.ug_generator import generateUg
 
+if len(sys.argv) <= 2:
+    print("Za mało argumentów.")
+    print("gen.py <nasycenie w %> <ilość elementów>")
+    exit(1)
+
 saturation = float(sys.argv[1]) / 100
 size = int(sys.argv[2])
 
-print("DAG:", timeit.timeit(lambda: generateDag(size, saturation), number=1))
-print(" UG:", timeit.timeit(lambda: generateUg(size, saturation), number=1))
+print("Skierowane:", timeit.timeit(lambda: generateDag(size, saturation), number=1), "sekund")
+print("Nieskierowane:", timeit.timeit(lambda: generateUg(size, saturation), number=1), "sekund")
