a75f54f15ec2f355fa13a42f666639f31a2372f5
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-06-09 10:31:49 +0200

    Wersja z pomiarami

--- 5. Plecak/plecak.py
+++ 5. Plecak/plecak.py
@@ -1,6 +1,8 @@
 from generator import generate
 from algos.greedy import greedy
 from algos.dynamic import dynamic
+from timeit import timeit
+import sys
 
 
 
@@ -17,22 +19,57 @@ def main():
     #     kontener = [sizes[i],values[i]]
     #     kontenery.append(kontener)
 
-    b = 100
-    kontenery = generate(b, 5*b, 10)
-    n = len(kontenery)
+    if(len(sys.argv) <= 3):
+        print("Sposób użycia:")
+        print("   ", sys.argv[0], "(pojemność statku) (całkowity rozmiar kontenerów) (współczynnik fragmentacji)")
+        exit(1)
 
-    greedy_ship = greedy(kontenery,n,b)
-    dynamic_ship = dynamic(kontenery,n,b)
+    b = int(sys.argv[1])  #1000
+    cont_total = int(sys.argv[2])  #2*b
+    size_factor = int(sys.argv[3])  #4
 
-    print("Zachłanny: ", greedy_ship)
-    print(count_value(greedy_ship))
-    print("Dynamiczny", dynamic_ship)
-    print(count_value(dynamic_ship))
+    repetitions = 10
+
+    time_d = 0
+    time_g = 0
+    avg_err = 0
+
+    for i in range(repetitions):
+        print("Przebieg", i+1)
+
+        kontenery = generate(b, cont_total, size_factor)
+        n = len(kontenery)
+        dynamic_ship = [ None ]
+        greedy_ship = [ None ]
+        time_d += timeit(lambda: invoke(dynamic, dynamic_ship, kontenery, n, b), number=1)
+        time_g += timeit(lambda: invoke(greedy, greedy_ship, kontenery, n, b), number=1)
+
+        D_gr = count_value(greedy_ship[0])
+        D_dyn = count_value(dynamic_ship[0])
+        avg_err += (D_dyn - D_gr) / D_dyn
+        print("Zach: ", D_gr, "  Dyn: ", D_dyn)
+    
+    time_d /= repetitions
+    time_g /= repetitions
+    avg_err /= repetitions
+
+    time_d = int(1e5 * time_d) / 100
+    time_g = int(1e5 * time_g) / 100
+    avg_err = int(1e4 * avg_err) / 100
+
+    print("Kontenery:", cont_total)
+    print("    Dynamiczny:", time_d, "ms")
+    print("     Zachłanny:", time_g, "ms")
+    print("          Błąd:", avg_err, "%")
+
 
 def count_value(containers):
     total = 0
     for cont in containers:
         total += cont[1]
     return total
+
+def invoke(func, ret, arg0, arg1, arg2):
+    ret[0] = func(arg0, arg1, arg2)
 
 main()
